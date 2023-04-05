8668f8f1cbf4d479a57c32e08b2e2b3d777814c7
Edgar Justavino <edgarja_26@hotmail.com>
2023-04-05 15:34:51 -0300

    feat: add module providers

--- currency-conversion/src/app.module.ts
+++ currency-conversion/src/app.module.ts
@@ -1,7 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
+import { GetExchangeRate } from './application/use-cases/get-exchange-rate';
+import { SqsConsumerService } from './infra/messaging/sqs/sqs-consumer.service';
+import { SqsProducerService } from './infra/messaging/sqs/sqs-producer.service';
 
 @Module({
   imports: [HttpModule],
+  providers: [GetExchangeRate, SqsConsumerService, SqsProducerService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  constructor(private readonly sqsConsumerService: SqsConsumerService) {}
+
+  async onApplicationBootstrap() {
+    await this.sqsConsumerService.receiveMessage();
+  }
+}
