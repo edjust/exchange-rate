b75487b82cc162a048b07c8f791b1df8a69a2298
Edgar Justavino <edgarja_26@hotmail.com>
2023-04-05 15:18:21 -0300

    feat: add env variables

--- currency-conversion/src/infra/messaging/sqs/sqs-producer.service.ts
+++ currency-conversion/src/infra/messaging/sqs/sqs-producer.service.ts
@@ -4,18 +4,18 @@ import { SQS } from 'aws-sdk';
 @Injectable()
 export class SqsProducerService {
   private sqs = new SQS({
-    region: '<your AWS region>',
-    accessKeyId: '<your AWS access key ID>',
-    secretAccessKey: '<your AWS secret access key>',
+    region: process.env.AWS_REGION || 'us-east-1',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   });
-  private readonly queueUrl: '';
+  private readonly queueUrl = process.env.AWS_QUEUE_URL || '';
 
   async sendMessage(messageBody: string): Promise<void> {
     const params = {
       MessageBody: messageBody,
       QueueUrl: this.queueUrl,
     };
-    console.log('SQS Message: ', params);
+    console.log('SQS Producer Message: ', params);
     // await this.sqs.sendMessage(params).promise();
   }
 }
